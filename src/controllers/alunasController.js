e02eb8bdc929c4826f05c4d568a6db0dee265359
taislopes <talopescontat@gmail.com>
2019-10-08 22:20:14 -0300

    Atualizações

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -10,3 +10,14 @@ exports.getById = (require, response) => {
     console.log(id)
     response.status(200).send(alunas.find(aluna => aluna.id == id))
 }
+
+exports.getBooks = (req, resp) => {
+    const id = req.params.id
+    const aluna = alunas.find(aluna => aluna.id == id)
+    console.log(aluna)
+    const livrosAluna = aluna.livros
+    const titulodLivros = livrosAluna.map(livros => livroDaAluna.titulo)
+
+    resp.status(200).send (titulodLivros)
+}
+
--- src/routes/alunasRoute.js
+++ src/routes/alunasRoute.js
@@ -4,5 +4,5 @@ const controller = require('../controllers/alunasController')
 
 router.get('/', controller.get)
 router.get('/:id',controller.getById)
-
+router.get ("/:id/books", controller.getBooks)
 module.exports = router
